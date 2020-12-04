a6515ef519de42e44707be3d36b069eef20059d1
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-12-04 17:48:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## What is this?
+
+Nothing at the moment.
+
+## Should I try this out?
+
+No.
---
+++ app/src/main/java/com/anime/dl/ui/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anime.dl.ui.main
+
+import android.app.Activity
+import android.os.Bundle
+import com.anime.dl.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstance: Bundle?) {
+        setContentView(R.layout.main)
+    }
+}
