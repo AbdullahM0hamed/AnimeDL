be3531dfadb8b9e46963c8c11b5e0bacc50273dd
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-12-29 14:22:08 +0000

    OnNavigationItemSelectedPlaceholder

--- app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
@@ -2,11 +2,23 @@ package com.anime.dl.ui.main
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import com.anime.dl.R
+import com.anime.dl.databinding.MainBinding
 
 class MainActivity : Activity() {
+
+    private lateinit var binding: MainBinding
+
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
-        setContentView(R.layout.main)
+        binding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            val toast = Toast.makeText(applicationContext, "test", 5)
+            toast.show()
+            true
+        }
     }
 }
