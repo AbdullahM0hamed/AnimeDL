eb0da637afae9eeb5a6362fccde14a525f20f151
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-12-31 14:36:39 +0000

    Add Router

--- app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import android.widget.Toast
 import com.anime.dl.R
 import com.anime.dl.databinding.MainBinding
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
 
 class MainActivity : Activity() {
 
     private lateinit var binding: MainBinding
+    private lateinit var router: Router
 
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
+
+        if (!isTaskRoot) {
+            finish()
+            return
+        }
+
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        router = Conductor.attachRouter(this, binding.controllerContainer, savedInstance)
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             val toast = Toast.makeText(applicationContext, "test", 5)
