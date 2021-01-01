e9a3b42773a8c65fe12fce813c51a4debd0f4b27
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-01-01 18:42:20 +0000

    Add Tabs to browse

--- app/src/main/kotlin/com/anime/dl/ui/browse/BrowseController.kt
+++ app/src/main/kotlin/com/anime/dl/ui/browse/BrowseController.kt
@@ -8,6 +8,10 @@ import com.anime.dl.R
 import com.anime.dl.databinding.BrowseControllerBinding
 import com.anime.dl.ui.base.controller.BaseController
 import com.anime.dl.ui.base.controller.TabbedController
+import com.anime.dl.ui.main.MainActivity
+import com.bluelinelabs.conductor.ControllerChangeHandler
+import com.bluelinelabs.conductor.ControllerChangeType
+import com.google.android.material.tabs.TabLayout
 
 class BrowseController : 
     BaseController<BrowseControllerBinding>(),
@@ -23,5 +27,21 @@ class BrowseController :
 
     override fun getTitle(): String? {
         return resources!!.getString(R.string.browse)
+    }
+
+    override onChangeStarted(handler: ControllerChangeHandler, type: ControllerChangeType) {
+        super.onChangeStarted(handler, type)
+        if (type.isEnter) {
+            (activity as? MainActivity)?.binding?.tabs?.apply {
+                setupWithViewPager(binding.pager)
+            }
+        }
+    }
+
+    override fun configureTabs(tabs: TabLayout) {
+        with(tabs) {
+            tabGravity = TabLayout.GRAVITY_FILL
+            tabMode = TabLayout.MODE_FIXED
+        }
     }
 }
