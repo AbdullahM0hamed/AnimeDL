a939a233f47f8b3f19daf2fbd616d2eec526ecca
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-04-28 17:49:19 +0100

    Change safe-call to non-null assertion

--- app/src/main/java/com/anime/dl/ui/anime/AnimeController.kt
+++ app/src/main/java/com/anime/dl/ui/anime/AnimeController.kt
@@ -58,8 +58,8 @@ class AnimeController : BaseController<AnimeControllerBinding> {
         super.onViewCreated(view)
 
         val context = App.applicationContext()
-        android.widget.Toast.makeText(context, (anime?.cover!! == "").toString(), 5).show()
-        if (anime?.cover!! != "") { 
+        android.widget.Toast.makeText(context, (anime!!.cover == "").toString(), 5).show()
+        if (anime!!.cover != "") { 
             Glide.with(context)
                 .load(anime?.cover)
                 .diskCacheStrategy(DiskCacheStrategy.DATA)
