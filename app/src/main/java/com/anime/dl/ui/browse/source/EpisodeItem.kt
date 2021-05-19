3f962e4cb33a5ee5bde9b3b8326249d54458be2c
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-05-19 18:10:07 +0100

    Switch between layouts in EpisodeItem

--- app/src/main/java/com/anime/dl/ui/browse/source/EpisodeItem.kt
+++ app/src/main/java/com/anime/dl/ui/browse/source/EpisodeItem.kt
@@ -3,9 +3,11 @@ package com.anime.dl.ui.browse.source
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.viewbinding.ViewBinding
 import com.anime.dl.App
 import com.anime.dl.R
 import com.anime.dl.databinding.EpisodeRowBinding
+import com.anime.dl.databinding.EpisodeRowWithThumbnailBinding
 import com.anime.dl.sources.models.EpisodeInfo
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -14,37 +16,43 @@ import com.mikepenz.fastadapter.binding.AbstractBindingItem
 
 data class EpisodeItem(
     val episode: EpisodeInfo
-) : AbstractBindingItem<EpisodeRowBinding>() {
+) : AbstractBindingItem<ViewBinding>() {
 
     override val type: Int = R.id.fastadapter_episode_item_id
-    private lateinit var binding: EpisodeRowBinding
+    private var binding: EpisodeRowBinding? = null
+    private var thumbnailBinding: EpisodeRowWithThumbnailBinding? = null
 
-    override fun bindView(binding: EpisodeRowBinding, payloads: List<Any>) {
-        this.binding = binding
-        binding.episodeTitle.text = episode.title
-        setImage(episode)
+    override fun bindView(binding: ViewBinding, payloads: List<Any>) {
+        if (!episode.thumbnail.isNullOrEmpty()) {
+            this.thumbnailBinding = binding
+            thumbnailBinding?.episodeTitle?.text = episode.title
+            setImage(episode)
+        } else {
+            this.binding = binding
+            binding?.episodeTitle?.text = episode.title
+        }
     }
 
     override fun createBinding(
         inflater: LayoutInflater,
         parent: ViewGroup?
-    ): EpisodeRowBinding {
-        return EpisodeRowBinding.inflate(inflater, parent, false)
+    ): ViewBinding {
+        return if (episode.thumbnail.isNullOrEmpty()) {
+            EpisodeRowBinding.inflate(inflater, parent, false)
+        } else {
+            EpisodeRowWithThumbnailBinding.inflate(inflater, parent, false)
+        }
     }
 
     fun setImage(episode: EpisodeInfo) {
         val context = App.applicationContext()
 
-        Glide.with(context).clear(binding.episodeThumbnail)
-        if (!episode.thumbnail.isNullOrEmpty()) {
-            Glide.with(context)
-                .load(episode.thumbnail)
-                .diskCacheStrategy(DiskCacheStrategy.DATA)
-                .centerCrop()
-                .placeholder(android.R.color.transparent)
-                .into(binding.episodeThumbnail)
-        } else {
-            binding.episodeThumbnail.isVisible = false
-        }
+        Glide.with(context).clear(thumbnailBinding.episodeThumbnail)
+        Glide.with(context)
+            .load(episode.thumbnail)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+            .centerCrop()
+            .placeholder(android.R.color.transparent)
+            .into(binding.episodeThumbnail)
     }
 }
