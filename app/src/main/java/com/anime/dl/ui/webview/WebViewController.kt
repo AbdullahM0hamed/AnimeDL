962cd7b3dd276b39bc649f1e89ebf2c791ccd3d7
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-05-23 22:02:09 +0100

    Add Webview V

--- app/src/main/java/com/anime/dl/ui/webview/WebViewController.kt
+++ app/src/main/java/com/anime/dl/ui/webview/WebViewController.kt
@@ -11,6 +11,7 @@ import android.webkit.WebViewClient
 import android.widget.Toast
 import com.anime.dl.R
 import com.anime.dl.databinding.WebviewControllerBinding
+import com.anime.dl.ui.base.controller.BaseController
 
 class WebViewController(
     val title: String?,
