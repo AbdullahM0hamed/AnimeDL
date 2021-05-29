410fd02710ed4d7a32bac3a93d303497253dff4d
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-05-29 20:24:43 +0100

    Add library controller II

--- app/src/main/java/com/anime/dl/ui/library/LibraryController.kt
+++ app/src/main/java/com/anime/dl/ui/library/LibraryController.kt
@@ -3,7 +3,6 @@ package com.anime.dl.ui.library
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import com.anime.dl.R
 import com.anime.dl.databinding.LibraryControllerBinding
 import com.anime.dl.ui.base.controller.BaseController
@@ -17,7 +16,6 @@ class LibraryController : BaseController<LibraryControllerBinding>() {
 
     override fun onViewCreated(view: View) {
         super.onViewCreated(view)
-        binding.emptyView.isVisible = true
     }
 
     override fun getTitle(): String? = resources!!.getString(R.string.home)
