d6cb8f897a3bda04ddd38f3674007730ca9577ae
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-09-01 18:45:43 +0100

    Advance by retreating

--- app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/anime/dl/ui/main/MainActivity.kt
@@ -4,23 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.anime.dl.R
-import com.anime.dl.databinding.ExtensionCardItemBinding
 import com.anime.dl.databinding.MainBinding
 import com.anime.dl.reducers.appStateReducer
 import com.anime.dl.states.AppState
 import com.anime.dl.ui.base.controller.PlaceholderController
 import com.anime.dl.ui.browse.BrowseController
-import com.anime.dl.ui.browse.extension.ExtensionItem
 import com.anime.dl.ui.library.LibraryController
 import com.anime.dl.ui.settings.SettingsController
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction.with
 import org.reduxkotlin.createThreadSafeStore
-import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence
-import uk.co.deanwild.materialshowcaseview.ShowcaseConfig
-import uk.co.deanwild.materialshowcaseview.shape.OvalShape
-import uk.co.deanwild.materialshowcaseview.shape.Shape
 
 val mainStore = createThreadSafeStore(::appStateReducer, AppState())
 
@@ -41,8 +35,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.topAppBar)
         router = Conductor.attachRouter(this, binding.controllerContainer, savedInstance)
-
-        tutorial()
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             val id = item.itemId
@@ -68,96 +60,5 @@ class MainActivity : AppCompatActivity() {
         if (!router.handleBack()) {
             super.onBackPressed()
         }
-    }
-
-    private lateinit var downloadExtButton: View
-    private lateinit var extensionBinding: ExtensionCardItemBinding
-    private lateinit var installExtListener: MaterialShowcaseSequence.OnSequenceItemDismissedListener
-    private val controller: BrowseController = BrowseController()
-
-    private fun tutorial() {
-        binding.bottomNavigation.selectedItemId = R.id.nav_browse
-        router.setRoot(with(controller))
-
-        installExtListener =
-            MaterialShowcaseSequence.OnSequenceItemDismissedListener() { _, _ ->
-                downloadExtButton.performClick()
-
-                tutorial(
-                    listOf(
-                        Pair(
-                            extensionBinding.root, R.string.tutorial_click_source
-                        )
-                    ),
-                    null,
-                    3,
-                    OvalShape()
-                )
-            }
-
-        extensionTutorial()
-    }
-
-    private fun extensionTutorial() {
-        // Displays extension download button
-        val extListener =
-            MaterialShowcaseSequence.OnSequenceItemDismissedListener() { _, _ ->
-                extensionBinding =
-                    (
-                        controller
-                            .itemAdapter
-                            .getAdapterItem(1) as ExtensionItem
-                        ).binding
-
-                downloadExtButton = extensionBinding.extButton
-
-                tutorial(
-                    listOf(
-                        Pair(downloadExtButton, R.string.tutorial_download_ext)
-                    ),
-                    installExtListener,
-                    2
-                )
-            }
-
-        tutorial(
-            listOf(
-                Pair(
-                    binding.bottomNavigation.findViewById(R.id.nav_browse),
-                    R.string.tutorial_browse
-                ),
-            ),
-            extListener,
-            1
-        )
-    }
-
-    private fun tutorial(
-        viewsAndTutorialStrings: List<Pair<View, Int>>,
-        listener: MaterialShowcaseSequence.OnSequenceItemDismissedListener?,
-        position: Int,
-        shape: Shape? = null
-    ) {
-        val config: ShowcaseConfig = ShowcaseConfig()
-        config.delay = 500
-        config.renderOverNavigationBar = true
-
-        if (shape != null) {
-            config.shape = shape
-        }
-
-        val sequence: MaterialShowcaseSequence =
-            MaterialShowcaseSequence(this@MainActivity, "tutorial_$position")
-        sequence.setConfig(config)
-
-        viewsAndTutorialStrings.forEach { item ->
-            sequence.addSequenceItem(
-                item.first, resources!!.getString(item.second), resources!!.getString(R.string.tutorial_understood)
-            )
-        }
-
-        sequence.setOnItemDismissedListener(listener)
-
-        sequence.start()
     }
 }
