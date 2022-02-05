05449013bbc75d8dda3ed6353c890d257ae5ca7b
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-02-05 00:31:34 +0000

    Why????????????

--- app/src/main/kotlin/com/anime/dl/sources/TutorialSource.kt
+++ app/src/main/kotlin/com/anime/dl/sources/TutorialSource.kt
@@ -24,7 +24,6 @@ class TutorialSource : HttpSource() {
 
     override fun browseAnimeFromJson(json: String): List<AnimeInfo>? {
         val array = JSONArray(json)
-        throw Exception(array.toString())
 
         return array.asJsonList().map { json ->
             AnimeInfo(
